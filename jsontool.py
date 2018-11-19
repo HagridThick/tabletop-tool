20a70bde6f4214ca9c93b9841e99e4362fce6240
徐晨鸥 <464154015@qq.com>
2018-11-19 14:57:17 +0800

    完成读取指定json文件，下载其中所有URL对象的功能，并且图片按照 tabletop simulatitor格式保存的功能。 可以勉强用用了，后续再优化与添加新功能

--- jsontool.py
+++ jsontool.py
@@ -21,7 +21,7 @@ def allprint(d):
             print(v)
 
 def print_url(d):
-    for k,v in d.items():
+    for k, v in d.items():
         if isinstance(v, dict):
             print_url(v)
         else:
@@ -30,7 +30,25 @@ def print_url(d):
                 #print(type(v))
                 if(uri_validator(v)):
                     print(v)
+
+def download_images(d):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            download_images(v)
+        else:
+            if (isinstance(v,str)):
+                #print(v)
+                #print(type(v))
+                if(uri_validator(v)):
+                    print(v)
+                    save_img(v,change_filename(v))
  
+def change_filename(file_name):
+    save_name = file_name.replace('/','')
+    save_name = save_name.replace(':','')
+    save_name = save_name.replace('.','')
+    return save_name
+
 def save_img(img_url,file_name,file_path='images'):
     #保存图片到磁盘文件夹 file_path中，默认为当前脚本运行目录下的 images 文件夹
     try:
@@ -40,8 +58,10 @@ def save_img(img_url,file_name,file_path='images'):
             os.makedirs(file_path)
         #获得图片后缀
         file_suffix = os.path.splitext(img_url)[1]
+        #print(file_suffix)   .jpg
         #拼接图片名（包含路径）
         filename = '{}{}{}{}'.format(file_path,os.sep,file_name,file_suffix)
+        print(filename)
        #下载图片，并保存到文件夹中
         urlretrieve(img_url,filename=filename)
     except IOError as e:
@@ -53,6 +73,10 @@ with open('776875059.json',encoding='utf-8') as f:
 
     data = json.load(f)
     #allprint(data)
-    print_url(data)
-    test_img ="https://img1.doubanio.com/view/subject/l/public/s29845508.jpg"
-    save_img(test_img,"test_img")
+    #print_url(data)
+
+    #test_img ="https://img1.doubanio.com/view/subject/l/public/s29845508.jpg"
+    #file_name = change_filename(test_img)
+    #save_img(test_img,file_name)
+
+    download_images(data)
